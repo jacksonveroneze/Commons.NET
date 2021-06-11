f26781676adda10e93a9e95442a440fc8edb6615
Jackson Veroneze <jackson@jacksonveroneze.com>
2021-06-10 22:17:35 -0300

    feat: realizado ajustes gerais

--- src/JacksonVeroneze.NET.Commons/Database/DatabaseConfiguration.cs
+++ src/JacksonVeroneze.NET.Commons/Database/DatabaseConfiguration.cs
@@ -9,89 +9,78 @@ namespace JacksonVeroneze.NET.Commons.Database
         public static IServiceCollection AddSqlServerDatabaseConfiguration<T>(this IServiceCollection services,
             Action<DatabaseOptions> action) where T : DbContext
         {
-            DatabaseOptions optionsConfig = new DatabaseOptions();
-
-            action?.Invoke(optionsConfig);
+            DatabaseOptions optionsConfig = FactoryOptions(action);
 
             return services.AddDbContext<T>((_, options) =>
-            {
-                options
-                    .UseSqlServer(optionsConfig.ConnectionString,
-                        optionsBuilder =>
-                        {
-                            optionsBuilder
-                                .CommandTimeout((int) TimeSpan.FromMinutes(3).TotalSeconds)
-                                .EnableRetryOnFailure(5, TimeSpan.FromSeconds(30), null);
-                        })
-                    .UseSnakeCaseNamingConvention();
-
-                if (optionsConfig.UseLazyLoadingProxies)
-                    options.UseLazyLoadingProxies();
-
-                if (optionsConfig.EnableDetailedErrors)
-                    options.EnableDetailedErrors();
-
-                if (optionsConfig.EnableSensitiveDataLogging)
-                    options.EnableSensitiveDataLogging();
-            });
+                options.UseSqlServer(optionsConfig.ConnectionString, optionsBuilder =>
+                        optionsBuilder
+                            .CommandTimeout((int) TimeSpan.FromMinutes(3).TotalSeconds)
+                            .EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)
+                    )
+                    .ConfigureOptions(optionsConfig)
+                    .ConfigureLogger(optionsConfig)
+            );
         }
 
         public static IServiceCollection AddPostgreSqlDatabaseConfiguration<T>(this IServiceCollection services,
             Action<DatabaseOptions> action) where T : DbContext
         {
-            DatabaseOptions optionsConfig = new DatabaseOptions();
-
-            action?.Invoke(optionsConfig);
+            DatabaseOptions optionsConfig = FactoryOptions(action);
 
             return services.AddDbContext<T>((_, options) =>
-            {
-                options
-                    .UseNpgsql(optionsConfig.ConnectionString,
-                        optionsBuilder =>
-                        {
-                            optionsBuilder
-                                .CommandTimeout((int) TimeSpan.FromMinutes(3).TotalSeconds)
-                                .EnableRetryOnFailure(5, TimeSpan.FromSeconds(30), null);
-                        })
-                    .UseSnakeCaseNamingConvention();
-
-                if (optionsConfig.UseLazyLoadingProxies)
-                    options.UseLazyLoadingProxies();
-
-                if (optionsConfig.EnableDetailedErrors)
-                    options.EnableDetailedErrors();
-
-                if (optionsConfig.EnableSensitiveDataLogging)
-                    options.EnableSensitiveDataLogging();
-            });
+                options.UseNpgsql(optionsConfig.ConnectionString, optionsBuilder =>
+                        optionsBuilder
+                            .CommandTimeout((int) TimeSpan.FromMinutes(3).TotalSeconds)
+                            .EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)
+                    )
+                    .ConfigureOptions(optionsConfig)
+                    .ConfigureLogger(optionsConfig)
+            );
         }
 
         public static IServiceCollection AddSqliteDatabaseConfiguration<T>(this IServiceCollection services,
             Action<DatabaseOptions> action) where T : DbContext
         {
+            DatabaseOptions optionsConfig = FactoryOptions(action);
+
+            return services.AddDbContext<T>((_, options) =>
+                options.UseSqlite(optionsConfig.ConnectionString, optionsBuilder =>
+                        optionsBuilder
+                            .CommandTimeout((int) TimeSpan.FromMinutes(3).TotalSeconds)
+                    )
+                    .ConfigureOptions(optionsConfig)
+                    .ConfigureLogger(optionsConfig)
+            );
+        }
+
+        private static DatabaseOptions FactoryOptions(Action<DatabaseOptions> action)
+        {
             DatabaseOptions optionsConfig = new DatabaseOptions();
 
             action?.Invoke(optionsConfig);
 
-            return services.AddDbContext<T>((_, options) =>
-            {
-                options
-                    .UseSqlite(optionsConfig.ConnectionString, optionsBuilder =>
-                    {
-                        optionsBuilder
-                            .CommandTimeout((int) TimeSpan.FromMinutes(3).TotalSeconds);
-                    })
-                    .UseSnakeCaseNamingConvention();
-
-                if (optionsConfig.UseLazyLoadingProxies)
-                    options.UseLazyLoadingProxies();
-
-                if (optionsConfig.EnableDetailedErrors)
-                    options.EnableDetailedErrors();
-
-                if (optionsConfig.EnableSensitiveDataLogging)
-                    options.EnableSensitiveDataLogging();
-            });
+            return optionsConfig;
         }
+
+        private static DbContextOptionsBuilder ConfigureOptions(this DbContextOptionsBuilder options,
+            DatabaseOptions optionsConfig)
+        {
+            options.UseSnakeCaseNamingConvention();
+
+            if (optionsConfig.UseLazyLoadingProxies)
+                options.UseLazyLoadingProxies();
+
+            if (optionsConfig.EnableDetailedErrors)
+                options.EnableDetailedErrors();
+
+            if (optionsConfig.EnableSensitiveDataLogging)
+                options.EnableSensitiveDataLogging();
+
+            return options;
+        }
+
+        private static DbContextOptionsBuilder ConfigureLogger(this DbContextOptionsBuilder options,
+            DatabaseOptions optionsConfig)
+            => options;
     }
 }
--- src/JacksonVeroneze.NET.Commons/Routing/RoutingConfiguration.cs
+++ src/JacksonVeroneze.NET.Commons/Routing/RoutingConfiguration.cs
@@ -5,6 +5,10 @@ namespace JacksonVeroneze.NET.Commons.Routing
     public static class RoutingConfiguration
     {
         public static IServiceCollection AddRoutingConfiguration(this IServiceCollection services)
-            => services.AddRouting(options => options.LowercaseUrls = true);
+            => services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+            });
     }
 }
